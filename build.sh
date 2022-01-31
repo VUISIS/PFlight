c033d0b34084fab24d5dfffa0460aaf7c2c4e794
Stephen Johnson <sajttu22@gmail.com>
2022-01-31 04:06:55 -0600

    Fixed build files to reflect submodule change.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd Ext/MAVSDK
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=../../build/MAVSDK/install -B../../build/MAVSDK -H.
 cmake --build ../../build/MAVSDK --target install
 
-cd ../PVUISIS/Src
+cd ../P/Src
 cmake -DCMAKE_INSTALL_PREFIX=../../../build/P/install -B../../../build/P -H.
 cmake --build ../../../build/P 
 
