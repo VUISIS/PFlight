d11d46f53c46442d473309073d14c8b612330cb0
Stephen Johnson <stephen.johnson@vanderbilt.edu>
2022-07-07 13:22:29 -0500

    Changed functions to async.

--- C/PForeign/FlightControllerFuncs.cpp
+++ C/PForeign/FlightControllerFuncs.cpp
@@ -122,7 +122,6 @@ PRT_VALUE* P_TelemetryHealthAllOk_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** ar
         if(health)
         {
             prom.set_value(true);
-            _telemetry->subscribe_health_all_ok(nullptr);
         }
     });
 
@@ -174,13 +173,37 @@ PRT_VALUE* P_UploadMission_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs)
 
 PRT_VALUE* P_SystemStatus_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs)
 {
-    return PrtMkBoolValue((PRT_BOOLEAN) _system->is_connected());
+    auto prom = std::promise<bool>{};
+    auto fut = prom.get_future();
+    _system->subscribe_is_connected([&prom](bool connected)
+    {
+        if(connected)
+        {
+            prom.set_value(true);
+        }
+    });
+
+    if (fut.wait_for(std::chrono::seconds(30)) == std::future_status::timeout) 
+	{
+        return PrtMkBoolValue((PRT_BOOLEAN)false);
+    }
+    return PrtMkBoolValue((PRT_BOOLEAN)fut.get());
 }
 
 PRT_VALUE* P_BatteryRemaining_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs)
 {
-    auto battery = _telemetry->battery();
-    return PrtMkFloatValue((PRT_FLOAT)battery.remaining_percent);
+    auto prom = std::promise<float>{};
+    auto fut = prom.get_future();
+    _telemetry->subscribe_battery([&prom](mavsdk::Battery battery)
+    {
+        prom.set_value(battery.remaining_percent);
+    });
+
+    if (fut.wait_for(std::chrono::seconds(30)) == std::future_status::timeout) 
+	{
+        return PrtMkBoolValue((PRT_BOOLEAN)false);
+    }
+    return PrtMkBoolValue((PRT_BOOLEAN)true);
 }
 
 PRT_VALUE* P_Holding_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs)
@@ -251,8 +274,20 @@ PRT_VALUE* P_WaitForDisarmed_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs
 
 PRT_VALUE* P_IsAtTakeoffAlt_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs)
 {
-    mavsdk::Telemetry::Position pos = _telemetry->position();
-    if(abs(pos.relative_altitude_m - 15.0f) < 0.5f)
+    auto prom = std::promise<float>{};
+    auto fut = prom.get_future();
+    _telemetry->subscribe_position([&prom](mavsdk::Position position)
+    {
+        prom.set_value(position.relative_altitude_m);
+    });
+
+    if (fut.wait_for(std::chrono::seconds(30)) == std::future_status::timeout) 
+	{
+        return PrtMkBoolValue((PRT_BOOLEAN)false);
+    }
+
+    auto val = fut.get();
+    if(abs(val - 15.0f) < 0.5f)
     {
         return PrtMkBoolValue(PRT_TRUE);
     }
@@ -300,7 +335,6 @@ PRT_VALUE* P_LandSystem_IMPL(PRT_MACHINEINST* context, PRT_VALUE*** argRefs)
     _action->land_async([](mavsdk::Action::Result res){ 
         if(res == mavsdk::Action::Result::Success)
         {
-            _action->land_async(nullptr);
         }
      });
 
